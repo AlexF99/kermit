c7574e7c01bd5751edf625555bbc48c42871420f
AlexF99 <alexandreopf@gmail.com>
2023-05-18 18:15:00 -0300

    client sending data and server receiving

--- client.c
+++ client.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include "socket.h"
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int socket = ConexaoRawSocket("lo");
+    unsigned char *buffer = (unsigned char *)malloc(8);
+    memset(buffer, 0, 8);
+    memcpy(buffer, "ale", 8);
+    for (;;)
+    {
+        sleep(1);
+        send(socket, buffer, htons(8), 0);
+        printf("%s\n", buffer);
+    }
     return 0;
 }
--- server.c
+++ server.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include "socket.h"
 
 int main(int argc, char const *argv[])
 {
-    int socket = ConexaoRawSocket("2175");
-    printf("socket: %d", socket);
+    int socket = ConexaoRawSocket("lo");
+
+    unsigned char *buffer = (unsigned char *)malloc(8); // to receive data
+    memset(buffer, 0, 8);
+
+    for (;;)
+    {
+        sleep(1);
+        recv(socket, buffer, htons(8), 0);
+        printf("%s\n", buffer);
+    }
     return 0;
 }
