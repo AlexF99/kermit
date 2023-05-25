0691800892bff9b8b25dbe799e7a598279d9b435
AlexF99 <alexandreopf@gmail.com>
2023-05-25 20:37:57 -0300

    lendo entradas e validando arquivos, 1 versao do cd local

--- client.c
+++ client.c
@@ -5,8 +5,10 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+
 #include "socket.h"
 #include "mensagem.h"
+#include "entrada.h"
 
 void print_byte(unsigned char c, int tam)
 {
@@ -17,28 +19,14 @@ void print_byte(unsigned char c, int tam)
 
 int main(int argc, char const *argv[])
 {
-    printf("operacoes:\ncd local: cd\nbackup: backup <nome arquivos ou regex>\n");
-    printf("recuperar backup: recupera <nome arquivos ou regex>\n");
-    printf("define dir p/ backup\nbkpdir <caminho>\nverifica backup: verifica <nome arquivo>\n");
-    char entrada[100];
-    char ch;
-
-    for (int loop = 0; (ch = getc(stdin)) != '\n';)
-    {
-        entrada[loop] = ch;
-        ++loop;
-    }
-
-    printf("entrada: %s\n", entrada);
-
     int socket = ConexaoRawSocket("lo");
     unsigned char *buffer = (unsigned char *)malloc(8);
     memset(buffer, 0, 8);
     memcpy(buffer, "ale", 8);
     for (;;)
     {
-        sleep(1);
-        send(socket, buffer, htons(8), 0);
+        get_entrada();
+        // send(socket, buffer, htons(8), 0);
     }
     return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 run:
+	gcc -Wall -c entrada.c -o entrada.o
 	gcc -Wall -c socket.c -o socket.o
 	gcc -Wall socket.o server.c -o server
-	gcc -Wall socket.o client.c -o client
+	gcc -Wall socket.o entrada.o client.c -o client
