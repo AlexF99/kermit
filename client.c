d2628a76653b8121a396efa6ea9e460224d98ff5
lgtg20 <lgtg20@inf.ufpr.br>
2023-05-26 11:54:37 -0300

    nome automático backup, backup mais de 4kb funcionando

--- client.c
+++ client.c
@@ -13,9 +13,10 @@ int main(int argc, char const *argv[])
 {
     FILE * arq;
     char a;
-    int n_mensagens = 1;
+    int n_mensagens;
     char dados[64];
-    char nome_arquivo[100];
+    char nome_arquivo[63];
+    size_t bytes_lidos;
 
     int socket = ConexaoRawSocket("lo");
     unsigned char *buffer = (unsigned char *)malloc(68);
@@ -36,24 +37,23 @@ int main(int argc, char const *argv[])
         }
         
         // Inicio da transmissão
-        msg = cria_mensagem(0, 0, BACKUP_ARQUIVO, 0);
+        msg = cria_mensagem(63, 0, BACKUP_ARQUIVO, 0);
+        memcpy(msg->dados, nome_arquivo, strlen(nome_arquivo));
+        n_mensagens = 0;
         pacote = empacota_mensagem(msg);
         memcpy(buffer, pacote, sizeof(unsigned char) * 68);
         send(socket, buffer, sizeof(unsigned char) * 68, 0);
+        n_mensagens++;
 
         while (!feof(arq))
         {
-            //sleep(1);
-            int i = 0;
-            while (i < 63 && !feof(arq))
-            {
-                a = getc(arq);
-                dados[i] = a;
-                ++i;
-            }
+            if (n_mensagens >= 64)
+                n_mensagens = 1;
 
-            msg = cria_mensagem((unsigned char)i, (unsigned char)n_mensagens, DADOS, 0);
-            memcpy(msg->dados, dados, i);
+            bytes_lidos = fread(dados, 1, 63, arq);
+
+            msg = cria_mensagem((unsigned char) bytes_lidos, (unsigned char)n_mensagens, DADOS, 0);
+            memcpy(msg->dados, dados, bytes_lidos);
             memset(dados, 0, 64);
             ++n_mensagens;
 
--- server.c
+++ server.c
@@ -16,6 +16,7 @@ int main(int argc, char const *argv[])
     int socket = ConexaoRawSocket("lo");
     mensagem_t * msg;
     FILE * arq;
+    char ext[10];
 
     unsigned char *buffer = (unsigned char *)malloc(68); // to receive data
     memset(buffer, 0, 68);
@@ -32,7 +33,18 @@ int main(int argc, char const *argv[])
             printf("Começando transmissão\n");
             counter = 1;
 
-            arq = fopen("backup", "w+");
+            char * ptr = strchr((char *) msg->dados, '.');
+
+            if (ptr)
+            {
+                memcpy(ext, ptr, strlen(ptr));
+                memcpy(ptr, "_backup", 7);
+                strcat((char *) msg->dados, ext);
+            }
+            else
+                strcat((char *) msg->dados, "_backup");
+
+            arq = fopen((char *) msg->dados, "w+");
 
             if (!arq)
             {
@@ -42,15 +54,17 @@ int main(int argc, char const *argv[])
 
             while (msg->tipo != FIM_ARQUIVO)
             {
+                if (counter >= 64)
+                    counter = 1;
+
                 recv(socket, buffer, sizeof(unsigned char) * 68, 0);
                 msg = desempacota_mensagem(buffer);
 
                 if (msg->sequencia == counter)
                 {
                     if (msg->tipo == DADOS)
-                    {
-                        fprintf(arq, "%s", (char *) msg->dados);
-                    }
+                        fwrite((char *) msg->dados, 1, msg->tamanho, arq);
+                        // fprintf(arq, "%s", (char *) msg->dados);
 
                     counter++;
                 }
